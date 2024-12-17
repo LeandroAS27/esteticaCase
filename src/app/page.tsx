b0a41e0be3c7f41e6c26d6511fd7af9cb5423f36
Leandro <leandro2791@hotmail.com>
2024-12-17 09:12:15 -0300

    criacao do projeto

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <header>
+
+      </header>
+
+      <main className="bg-[#F7F1E9] w-full h-screen">
+      </main>
+
+      <footer>
+
+      </footer>
+    </>
+  );
+}
