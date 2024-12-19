ee6f63801aa0732b1e9560209db67faa11433dc1
Leandro <leandro2791@hotmail.com>
2024-12-19 14:50:29 -0300

    Ajustado o footer na responsividade e adicionado o endereco, e ajustado o header

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -1,8 +1,9 @@
 const Navigation = () => {
     return(
         <>
-            <header id="home" className="w-full h-24 bg-[#A8CABA] flex justify-center items-center">
-                <nav>
+            <header id="home" className="w-full h-auto bg-[#A8CABA] flex flex-col sm:flex-row sm:justify-between items-center px-6 sm:px-12 py-4 gap-4 sm:gap-0">
+                <h1 className="font-bodoni text-[#4A4A4A] text-xl py-2">EsteticaCaseOne</h1>
+                <nav className="w-full sm:w-auto">
                     <ul className="flex flex-wrap justify-center sm:justify-between items-center gap-4 text-[#4A4A4A] font-bodoni text-md font-bold">
                         <li>
                             <a href="#home" className="hover:text-white">Home</a>
@@ -21,6 +22,9 @@ const Navigation = () => {
                         </li>
                     </ul>
                 </nav>
+                <div>
+                    <p className="font-nunito text-gray-100">Aesthetic & Wellness Spa</p>
+                </div>
             </header>
         </>
     )
